495ae5496a5817ee494739f28966dacbd7382e2e
cdyfng <xiaofeng_chen2007@163.com>
2020-03-26 09:14:54 +0800

    add necessary files to run

---
+++ cmd.sh
@@ -0,0 +1,6 @@
+#install parity 
+# https://github.com/paritytech/homebrew-paritytech.git 
+# if use kovan-testnet, you must install parity as a node( go-ethereum doesnot work)
+brew tap paritytech/paritytech
+brew install parity
+
---
+++ my_environment_example.sh
@@ -0,0 +1,4 @@
+SERVER_ETH_RPC_HOST=https://127.0.0.1
+SERVER_ETH_RPC_PORT=8545
+ACCOUNT_ADDRESS=axxxx30
+ACCOUNT_KEY="key_file=/Users/xxx0,pass_file=/Users/xxxx/pass"
